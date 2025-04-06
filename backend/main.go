0d5d66f262613767ad815b1e2293e7d46d549a8b
alexprimak24 <aprimak43@gmail.com>
2025-04-07 01:43:19 +0300

    first commit

---
+++ backend/main.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"database/sql"
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/gorilla/mux"
+	_ "github.com/lib/pq"
+)
+
+type User struct {
+	Id		int		`json:"id"`
+	Name	string	`json:"name"`
+	Email	string	`json:"email"`
+}
+
+//main function
+func main() {
+	//connect to database
+	db, err := sql.Open("postgres", os.Getenv("DATABASE_URL"))
+	if err != nil {
+		log.Fatal(err)
+	}
+	// close the connection
+	defer db.Close()
+}
