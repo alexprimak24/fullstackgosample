3dfd2b6e5ddf24a5c539b370430da21bace3b0d7
alexprimak24 <aprimak43@gmail.com>
2025-04-07 16:37:12 +0300

    Frontend is ready

---
+++ frontend/src/pages/index.tsx
@@ -0,0 +1,12 @@
+import UserInterface from "@/components/UserInterface";
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col items-center justify-between min-h-screen py-2">
+      <div className="m-4">
+      <UserInterface backendName="go"/>
+      </div>
+    </div>
+  );
+}
